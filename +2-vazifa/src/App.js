6a8121b0e3571a61c1594bc41a093cb7475a05ab
Juratbekdasturchi <juratbekgithubabduraimov@gmail.com>
2024-02-28 14:39:24 +0500

    Goodby

--- +2-vazifa/src/App.js
+++ +2-vazifa/src/App.js
@@ -26,7 +26,7 @@ function App() {
           </ul>
         </div>
 
-<div className="rasmlar">
+{/* <div className="rasmlar">
           <img src="https://static.wixstatic.com/media/d78df6_4517dc1e0b1545bf84dccd915203c870.jpg/v1/fill/w_526,h_526,al_c,lg_1,q_80,enc_auto/d78df6_4517dc1e0b1545bf84dccd915203c870.jpg" alt="" />
           <img src="https://static.wixstatic.com/media/d78df6_9f4f79dd27814710b261e3acb931156e.jpg/v1/fill/w_351,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_9f4f79dd27814710b261e3acb931156e.jpg" alt="" />
           <img src="https://static.wixstatic.com/media/d78df6_914d12d64a57420c99737b9933ebe7fc.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_914d12d64a57420c99737b9933ebe7fc.jpg" alt="" />
@@ -36,7 +36,7 @@ function App() {
           <img src="https://static.wixstatic.com/media/d78df6_b9747effc3074458a295cf5674f8c73c.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_b9747effc3074458a295cf5674f8c73c.jpg" alt="" />
           <img src="https://static.wixstatic.com/media/d78df6_24dc2b5011604bc7ada79cbd953a7c5a.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_24dc2b5011604bc7ada79cbd953a7c5a.jpg" alt="" />
           <img src="https://static.wixstatic.com/media/d78df6_394a136048754355ad8432b30522c761.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_394a136048754355ad8432b30522c761.jpg" alt="" />
-</div>
+</div> */}
 
       </main>
     </div>
